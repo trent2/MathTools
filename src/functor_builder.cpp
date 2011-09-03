a2b8e05610381e5d3ab959300c6c190f2130a1f4
Robert Spillner <Robert.Spillner@gmx.de>
2011-09-03 12:12:23 +0200

    - re-added abs, atan; added acos, asin

--- src/functor_builder.cpp
+++ src/functor_builder.cpp
@@ -22,11 +22,29 @@
 #include <exception>
 #include "functor.hpp"
 
+namespace std {
+  const std::complex<double> M_I = std::complex<double>(0,1);
+
+  inline std::complex<double> asin(const std::complex<double> &c) {
+    return -M_I*log(M_I*c+sqrt(1.0-c*c));
+  }
+
+  inline std::complex<double> acos(const std::complex<double> &c) {
+    return -M_I*log(c+M_I*sqrt(1.0-c*c));
+  }
+
+  inline std::complex<double> atan(const std::complex<double> &c) {
+    return log((1.0+M_I*c)/(1.0-M_I*c))/(2.0*M_I);
+  }
+
+  inline std::complex<double> abs(const std::complex<double> &c) {
+    return std::abs(c);
+  }
+}
+
 namespace functor {
   // extern const_unary_function<double>*         ___produceI(const double& );
   // extern const_unary_function<std::complex<double> >* ___produceI(const std::complex<double> &);
-
-  const std::complex<double> M_I = std::complex<double>(0,1);
 
   inline const_unary_function<double>*
   ___produceI(const double& ) {
@@ -35,8 +53,9 @@ namespace functor {
 
   inline const_unary_function<std::complex<double> >*
   ___produceI(const std::complex<double> &) {
-    return new const_unary_function<std::complex<double> >(M_I);
+    return new const_unary_function<std::complex<double> >(std::M_I);
   }
+
 
   template <class T>
   typename un_fun<T>::type* numeric_function_builder<T>::create_func(const GiNaC::ex &e) {
@@ -67,9 +86,13 @@ namespace functor {
       else if(fn=="cos")
 	return compose1<T>(ptr_fun<T>(std::cos), create_func(e.op(0)));
       else if(fn=="tan")
-	return compose1<T>(ptr_fun<T>(std::tan), create_func(e.op(0)));
-      // else if(fn=="atan")
-      //  	return compose1<T>(ptr_fun<T>(std::atan), create_func(e.op(0)));
+       	return compose1<T>(ptr_fun<T>(std::tan), create_func(e.op(0)));
+      else if(fn=="asin")
+	return compose1<T>(ptr_fun<T>(std::asin), create_func(e.op(0)));
+      else if(fn=="acos")
+	return compose1<T>(ptr_fun<T>(std::acos), create_func(e.op(0)));
+      else if(fn=="atan")
+	return compose1<T>(ptr_fun<T>(std::atan), create_func(e.op(0)));
       else if(fn=="exp")
 	return compose1<T>(ptr_fun<T>(std::exp), create_func(e.op(0)));
       else if(fn=="log")
@@ -82,10 +105,10 @@ namespace functor {
 	v->push_back(new const_unary_function<T>(std::log(10)));
 	return compose<T>(new divides<T>(), v);
       }
+      else if(fn=="abs")
+	return compose1<T>(ptr_fun<T>(std::abs), create_func(e.op(0)));
       else if(fn=="sqrt")
 	return compose1<T>(ptr_fun<T>(std::sqrt), create_func(e.op(0)));
-      // else if(fn=="abs")
-      //  	return compose1<T>(ptr_fun<T>(std::abs), create_func(e.op(0)));
       else if(fn=="D")
 	return new derivative<T>(create_func(e.op(0)));
     }
